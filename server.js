b22d626a984675316a0bcea9873a159c2f34298a
Tushar-Sehgal <90881321+Tushar-Sehgal@users.noreply.github.com>
2024-10-10 04:13:30 +1100

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,9 @@ app.use(express.json());
 
 // MongoDB Atlas connection (replace with your MongoDB Atlas connection string)
 const mongoURI = 'mongodb+srv://tstusharsehgal123:Y97oGYIANmR1NbZx@cluster0.l6xhj.mongodb.net/sensordata?retryWrites=true&w=majority&appName=Cluster0';
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB Atlas'))
-.catch((error) => console.error('MongoDB connection error:', error));
+mongoose.connect(mongoURI)
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch((error) => console.error('MongoDB connection error:', error));
 
 // Define a schema for sensor data
 const sensorSchema = new mongoose.Schema({
@@ -71,16 +68,16 @@ app.put('/sensor/:id', async (req, res) => {
 
 // Delete sensor data by ID (DELETE)
 app.delete('/sensor/:id', async (req, res) => {
-  try {
-    const sensor = await Sensor.findByIdAndDelete(req.params.id);
-    if (!sensor) return res.status(404).send('Sensor data not found');
-    res.send(sensor);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    try {
+      const sensor = await Sensor.findByIdAndDelete(req.params.id);
+      if (!sensor) return res.status(404).send('Sensor data not found');
+      res.send(sensor);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  });
+  
+  // Start the server
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
